b9c01d66a5d20ba965cb5c62f3d6672c784a133d
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-13 11:26:50 -0500

    8_Functions

---
+++ 8_Functions/index.ts
@@ -0,0 +1,18 @@
+// turn on in tsconfid.json
+// noUnusedParameters
+// noImplicitReturns
+// noUnusedLocals
+
+// : after() tells what kind of number to return
+// instead of , in thousands, use _
+// ? makes it optional ie: taxYear?
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022) 
+        return income * 1.2;
+    return income * 1.3;
+    
+}
+
+// normally you could add another parameter, ie: calculateTaxYear(10,000, 20)
+// taxYear is already defined above
+calculateTax(10_000);
