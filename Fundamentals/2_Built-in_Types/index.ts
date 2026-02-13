f6c556d791950bda6ef7705bf297406e41c5bb93
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-12 22:22:41 -0500

    2_Built_In_Types

---
+++ Fundamentals/2_Built-in_Types/index.ts
@@ -0,0 +1,7 @@
+// annotates type of number can seperate numbers with _
+let sales: number = 123_456_789;
+let course: string = 'Typescript';
+let is_published: boolean = true;
+
+// does not need annotation but does not make it strict
+let number = 2134;
