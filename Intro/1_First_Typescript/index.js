cad4bd8d18a3e80ad06065329e0b835b3053f3a7
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-12 21:08:12 -0500

    Made my first Typescript File

---
+++ Intro/1_First_Typescript/index.js
@@ -0,0 +1,3 @@
+console.log("Hello world");
+// static - keep it as a number instead of it being redefeined
+var age = 20;
---
+++ Intro/1_First_Typescript/index.ts
@@ -0,0 +1,4 @@
+console.log("Hello world");
+
+// static - keep it as a number instead of it being redefeined
+let age: number = 20;
