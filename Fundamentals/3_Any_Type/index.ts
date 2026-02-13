07e08d4187eeda26b900dd3fce09a4913aeab070
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-12 22:42:07 -0500

    3_Any_Type

---
+++ Fundamentals/3_Any_Type/index.ts
@@ -0,0 +1,11 @@
+
+// annotates type of number can seperate numbers with _
+let sales: number = 123_456_789;
+let course: string = 'Typescript';
+let is_published: boolean = true;
+
+// any - against entire valaue of typescript - avoid using
+let level;
+level = 1;
+level = 'a';
+
