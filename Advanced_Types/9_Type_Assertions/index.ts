3f1ab00dbc79e8aadb4b1cee216ac51fc69136dc
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-13 22:40:40 -0500

    9_Type_Assertions

---
+++ Advanced_Types/9_Type_Assertions/index.ts
@@ -0,0 +1,6 @@
+let phone = <HTMLInputElement> document.getElementById('phone') as HTMLInputElement;
+
+// HTMLElement - represents any kind of HTML element
+// HTMLInputElement, ect
+
+phone.value;
