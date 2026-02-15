ccec90b72849441b9d9fc6d47349b5061ca1fb88
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-14 22:03:10 -0500

    10_Static_Members

---
+++ Object_Oriented_Programming/10_Static_Members/index.ts
@@ -0,0 +1,24 @@
+class Ride {
+
+    // when making property static that method becomes part of 
+    // a class and will have only a single instance of them in memory
+
+    private static _activeRides: number = 0;
+
+    start() { Ride._activeRides++ };
+    stop() { Ride._activeRides-- };
+
+    // Getter 
+     static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+
+
+let ride1 = new Ride()
+ride1.start();
+
+let ride2 = new Ride()
+ride1.start();
+
+console.log(Ride.activeRides);
