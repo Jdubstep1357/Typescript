c96767ee76de4b749c90063fa8882b93266f1f11
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-14 22:14:10 -0500

    10_Static_Members

--- Object_Oriented_Programming/10_Static_Members/index.js
+++ Object_Oriented_Programming/10_Static_Members/index.js
@@ -1,11 +1,21 @@
 "use strict";
-// We can use properties to object dynamically
 Object.defineProperty(exports, "__esModule", { value: true });
-// trying to figure out who sits where
-class SeatAssignment {
+class Ride {
+    // when making property static that method becomes part of 
+    // a class and will have only a single instance of them in memory
+    static _activeRides = 0;
+    start() { Ride._activeRides++; }
+    ;
+    stop() { Ride._activeRides--; }
+    ;
+    // Getter 
+    static get activeRides() {
+        return Ride._activeRides;
+    }
 }
-let seats = new SeatAssignment();
-seats.A1 = 'Mary';
-seats['A1'] = 'John';
-seats.A2 = 'Carrie';
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride1.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
---
+++ Object_Oriented_Programming/11_Inheritance/index.ts
@@ -0,0 +1,44 @@
+/*
+Student
+Properties - FName, LName, FullName
+Methods - Walk(), Talk()
+
+Teacher
+Properties - FName, LName, FullName
+Methods - Walk(), Talk()
+
+Both Student and Teacher have same properties and methods
+better to have inheritance than writing code twice
+
+Have parent class Person
+
+*/
+
+class Person {
+    constructor(public firstName: string, public lastname: string) {}
+
+    get fullName() {
+        return this.firstName + ' ' + this.lastname;
+    }
+
+    walk() {
+        console.log('walking');
+    }
+}
+
+// extends tells class Students to inherit everythign from Person
+class Students extends Person {
+    // public only references studentId which is unique to class Students
+    // use super to reference constructor of base / parent class
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        // super class
+        super(firstName, lastName);
+    }
+
+    takeTest() {
+        console.log('Taking test');
+        
+    }
+}
+
+let student = new Students(1, 'John', 'doe@gmail.com');
