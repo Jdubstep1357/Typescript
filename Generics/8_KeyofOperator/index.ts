1a57fae4bd8bf6ebb8a8b1f9e8523a0d61fc8e8f
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-15 18:38:26 -0500

    8_KeyofOperator

---
+++ Generics/8_KeyofOperator/index.ts
@@ -0,0 +1,26 @@
+interface Product {
+    name: string;
+    price: number;
+}
+
+class Store<T> {
+    protected _objects: T[] = [];
+
+    add(obj: T): void {
+        this._objects.push(obj);
+    }
+
+    // T is Product
+    //keyof T => 'name' | 'price'
+    find(property: keyof T, value: unknown): T | undefined {
+        return this._objects.find(obj => obj[property] === value);
+    }
+}
+
+
+let store  = new Store<Product>();
+store.add({ name: 'a', price: 1});
+store.find('name', 'a');
+store.find('name', 1);
+// use key of operator to catch problem
+store.find('noExistingProperty', 1);
