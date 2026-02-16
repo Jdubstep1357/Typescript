e0966c31f8028ddf09deea0232ed929886e5ce49
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-15 20:50:13 -0500

    5_Decorator_Composition

---
+++ Decorators/5_Decorator_Composition/index.ts
@@ -0,0 +1,29 @@
+type ComponentOptions = {
+    selector: string;
+}
+
+
+function Component(options: ComponentOptions) {
+    return (constructor: Function) => {
+        console.log('Component decorator called');
+        constructor.prototype.uniqueId = Date.now();
+        constructor.prototype.insertInDOM = () => {
+            console.log('Inserting the component in the DOM');
+        }
+    }
+}
+
+
+function Pipe(constructor: Function) {
+    console.log('Pipe decorator called');
+    constructor.prototype.pipe = true; 
+}
+
+@Component({ selector: '#my-profile' })
+@Pipe 
+
+
+
+class ProfileComponent {
+
+}
