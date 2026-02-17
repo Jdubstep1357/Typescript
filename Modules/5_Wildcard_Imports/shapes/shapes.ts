7b52e53fcdb6aaa18421aaf1ce4a75342f2f14cd
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-17 18:24:53 -0500

    6_Re-exporting

---
+++ Modules/5_Wildcard_Imports/shapes/shapes.ts
@@ -0,0 +1,9 @@
+// split module to make it easier to maintain
+
+export class Circle {
+    constructor(public radius: number) { }
+}
+
+export class Square {
+    constructor(public width: number) {}
+}
